a52b2ebdef38414cfb1af4a9cd004ba8ba47ecd5
Shane Moore <shane@shanemoore.me>
2023-03-09 23:41:17 +0000

    Fetch main before checking diff

--- scripts/if-no-version-change.sh
+++ scripts/if-no-version-change.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if git diff main -- package.json | grep -e '^+ *"version":'
+if git diff origin/main -- package.json | grep -e '^+ *"version":'
 then
   echo "Not checking version; version was changed in this branch"
 else
