c28e130eaba147d529ccbe9d34440b5b4372b255
Randall Leeds <randall.leeds@nytimes.com>
2024-02-12 09:49:34 -0800

    Stop exporting the low-level building blocks of the main API
    Until APIs stabilize and users demonstrate a need for more control,
    remove the layout group and low-level editor exports.

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,6 @@
 
 export { ProseMirror } from "./components/ProseMirror.js";
 
-export { Editor } from "./components/Editor.js";
-export type { EditorProps } from "./components/Editor.js";
-
-export { EditorContext } from "./components/EditorContext.js";
-export type { EditorContextValue } from "./components/EditorContext.js";
-
-export { LayoutGroup } from "./components/LayoutGroup.js";
-export type { LayoutGroupProps } from "./components/LayoutGroup.js";
-
-export { LayoutGroupContext } from "./components/LayoutGroupContext.js";
-export type { LayoutGroupContextValue } from "./components/LayoutGroupContext.js";
-
-export { NodeViewsContext } from "./components/NodeViewsContext.js";
-export type { NodeViewsContextValue } from "./components/NodeViewsContext.js";
-
 export { useEditorEffect } from "./hooks/useEditorEffect.js";
 
 export { useEditorEventCallback } from "./hooks/useEditorEventCallback.js";
@@ -24,10 +9,6 @@ export { useEditorEventCallback } from "./hooks/useEditorEventCallback.js";
 export { useEditorEventListener } from "./hooks/useEditorEventListener.js";
 
 export { useEditorState } from "./hooks/useEditorState.js";
-
-export { useEditorView } from "./hooks/useEditorView.js";
-
-export { useLayoutGroupEffect } from "./hooks/useLayoutGroupEffect.js";
 
 export { useNodePos } from "./hooks/useNodePos.js";
 
