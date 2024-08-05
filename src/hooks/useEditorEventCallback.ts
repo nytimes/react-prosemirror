4a704d6f7684c6d351231932ae8956d29f07fec7
Brian Hung <brianlhung@gmail.com>
2024-08-05 16:58:36 -0700

    return value of callback

--- src/hooks/useEditorEventCallback.ts
+++ src/hooks/useEditorEventCallback.ts
@@ -30,7 +30,7 @@ export function useEditorEventCallback<T extends unknown[], R>(
 
   return useCallback(
     (...args: T) => {
-      if (editorView) ref.current(editorView, ...args);
+      if (editorView) return ref.current(editorView, ...args);
     },
     [editorView]
   );
