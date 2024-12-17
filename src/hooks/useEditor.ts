d019c089bdaea84a22d83c5337055676cb8d78d0
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-17 16:15:18 -0500

    Add a gnarly hack to prevent failing on window access during SSR

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -73,6 +73,13 @@ export class ReactEditorView extends EditorView {
     // Call the superclass constructor with an empty
     // document and limited props. We'll set everything
     // else ourselves.
+    const prevWindow = globalThis.window;
+    // @ts-expect-error HACK - EditorView checks for window.MutationObserver
+    // in its constructor, which breaks SSR. We temporarily set window
+    // to an empty object to prevent an error from being thrown, and then
+    // clean it up so that other isomorphic code doesn't get confused about
+    // whether there's a functioning global window object
+    globalThis.window ??= {};
     super(place, {
       state: EditorState.create({
         schema: props.state.schema,
@@ -80,6 +87,7 @@ export class ReactEditorView extends EditorView {
       }),
       plugins: props.plugins,
     });
+    globalThis.window = prevWindow;
 
     this.shouldUpdatePluginViews = true;
 
@@ -339,6 +347,11 @@ export function useEditor<T extends HTMLElement = HTMLElement>(
       return;
     }
 
+    // TODO: We should be able to put this in previous branch,
+    // but we need to convince EditorView's constructor not to
+    // clear out the DOM when passed a mount that already has
+    // content in it, otherwise React blows up when it tries
+    // to clean up.
     if (view.needsRedraw) {
       setView(null);
       return;
