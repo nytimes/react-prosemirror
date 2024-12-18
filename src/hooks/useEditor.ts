1f66f11958db19add47226349780849c1290a54e
Shane Friedman <smoores-gpg@friedmans.us>
2024-12-18 17:38:40 -0500

    Clean up layout effects in useEditor

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -326,39 +326,31 @@ export function useEditor<T extends HTMLElement = HTMLElement>(
   // so this is not a concern.
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useLayoutEffect(() => {
-    if (view && view.dom !== mount) {
-      setView(null);
-    }
-
     if (!mount) {
+      setView(null);
       return;
     }
 
-    if (!view) {
+    if (!view || view.dom !== mount) {
       const newView = new ReactEditorView({ mount }, directEditorProps);
       setView(newView);
       newView.dom.addEventListener("compositionend", forceUpdate);
       return;
     }
-  });
 
-  // This rule is concerned about infinite updates due to the
-  // call to setView. These calls are deliberately conditional,
-  // so this is not a concern.
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useLayoutEffect(() => {
-    // If ProseMirror View is about to redraw the entire document's
-    // DOM, clear the EditorView and reconstruct another, instead.
-    // This only happens when a newly instantiated EditorState has
-    // been provided.
-    if (view?.needsRedraw) {
+    // TODO: We should be able to put this in previous branch,
+    // but we need to convince EditorView's constructor not to
+    // clear out the DOM when passed a mount that already has
+    // content in it, otherwise React blows up when it tries
+    // to clean up.
+    if (view.needsRedraw) {
       setView(null);
       return;
-    } else {
-      // @ts-expect-error Internal property - domObserver
-      view?.domObserver.selectionToDOM();
-      view?.runPendingEffects();
     }
+
+    // @ts-expect-error Internal property - domObserver
+    view?.domObserver.selectionToDOM();
+    view?.runPendingEffects();
   });
 
   view?.pureSetProps(directEditorProps);
